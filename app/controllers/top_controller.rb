2d33732ef0a43aebfa603576007fa2e682cbff5e
soe-j <soeda@val.co.jp>
2016-12-03 05:01:25 +0900

    ランダムにデフォルト色入れとく

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
 
   def index
     gon.rosenz_key = Rails.configuration.rosenz_key
+    @default_color = %w(black red blue green purple orange).sample
   end
 
 end
