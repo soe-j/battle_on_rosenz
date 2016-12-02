ed816ee57d36124b36f2906ebe64f8f1b858b2de
soe-j <soeda@val.co.jp>
2016-12-03 05:31:13 +0900

    受け取ったtake情報をbroadcast

--- app/channels/battle_channel.rb
+++ app/channels/battle_channel.rb
@@ -1,13 +1,14 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class BattleChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "battle_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def take
+  def take(data)
+    ActionCable.server.broadcast 'battle_channel', stationCode: data['stationCode'], color: data['color']
   end
 end
