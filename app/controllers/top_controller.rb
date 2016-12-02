aa60b3c16e75f54ff19a2e5525c70c86103461cb
soe-j <soeda@val.co.jp>
2016-12-03 01:13:43 +0900

    indexページで路線図key確認

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,7 @@
 class TopController < ApplicationController
+
+  def index
+
+  end
+
 end
---
+++ config/initializers/rosen.rb
@@ -0,0 +1,4 @@
+# 路線図設定ファイル
+
+Rails.application.config.rosenz_key = ENV['BATTLE_ROSENZ_KEY']
+Rails.application.config.rosenz_url = ENV['BATTLE_ROSENZ_URL']
