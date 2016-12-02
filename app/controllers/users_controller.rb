9956911728326b01d2c693f1775a861b2b6e7ea3
soe-j <soeda@val.co.jp>
2016-12-03 03:43:05 +0900

    Userを作成するだけのcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    sign_in @user
+    render json: @user
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :color)
+  end
+
 end
