c799f155271b880b5bdc273be65bc0a3ed828135
soe-j <soeda@val.co.jp>
2016-12-03 04:04:29 +0900

    user index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   protect_from_forgery except: [:create]
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def create
     @user = User.create(user_params)
     sign_in @user
