0d5a622c32307f4a5f83b5969e0a03d26226d1cc
soe-j <soeda@val.co.jp>
2016-12-03 15:51:45 +0900

    userdataから環境変数を設定

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -1,5 +1,12 @@
 #/bin/bash -eu
 
+USERDATA=`curl -f http://169.254.169.254/latest/user-data`
+export SECRET_KEY_BASE=`echo ${USERDATA} | jq -r '.SECRET_KEY_BASE'`
+export BATTLE_ROSENZ_KEY=`echo ${USERDATA} | jq -r '.BATTLE_ROSENZ_KEY'`
+export BATTLE_ROSENZ_URL=`echo ${USERDATA} | jq -r '.BATTLE_ROSENZ_URL'`
+export BATTLE_ROSENZ_USER=`echo ${USERDATA} | jq -r '.BATTLE_ROSENZ_USER'`
+export BATTLE_ROSENZ_PASS=`echo ${USERDATA} | jq -r '.BATTLE_ROSENZ_PASS'`
+
 ENV=production
 
 source ~/.bash_profile
