cd96ab40cacaf4e4cb1e1281fa91443562f246df
soe-j <soeda@val.co.jp>
2016-12-03 11:20:41 +0900

    全Territory情報の必要な分を画面読み込み時に得る

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
   def index
     gon.rosenz_key = Rails.configuration.rosenz_key
     @default_color = %w(black red blue green purple orange).sample
+    gon.territories = Territory.all_status
   end
 
 end
--- app/models/territory.rb
+++ app/models/territory.rb
@@ -1,3 +1,9 @@
 class Territory < ApplicationRecord
   after_create_commit { TerritoryBroadcastJob.perform_later self }
+
+  def Territory.all_status
+    Territory.select('station_code', 'color').map do |trtr|
+      {stationCode: trtr.station_code, color: trtr.color}
+    end
+  end
 end
