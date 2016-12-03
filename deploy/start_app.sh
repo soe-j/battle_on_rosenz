cac3c9963e52247e7dd6ed6f8adaa99706bc2de5
soe-j <soeda@val.co.jp>
2016-12-03 16:25:57 +0900

    rails deamon起動

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -15,4 +15,4 @@ cd /service/battle_on_rosenz/
 bundle install -j4
 bundle exec rake assets:precompile RAILS_ENV=${ENV}
 mkdir /service/battle_on_rosenz/tmp/pids
-bundle exec rails s -e ${ENV}
+bundle exec rails s -e ${ENV} -d
