97f0bb7007e08d0ebce4f2031806eb91f2e7e7e2
soe-j <soeda@val.co.jp>
2016-12-03 21:17:14 +0900

    g player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203085020) do
+ActiveRecord::Schema.define(version: 20161203120140) do
+
+  create_table "players", force: :cascade do |t|
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "takeable_stations", force: :cascade do |t|
     t.integer  "code"
