ac0474d6aab42ad2ef956af0a2a08367dbea2bcf
soe-j <soeda@val.co.jp>
2016-12-03 16:33:51 +0900

    startシェルにdb migrate追加

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -14,5 +14,6 @@ source ~/.bash_profile
 cd /service/battle_on_rosenz/
 bundle install -j4
 bundle exec rake assets:precompile RAILS_ENV=${ENV}
+bundle exec rake db:migrate RAILS_ENV=${ENV}
 mkdir /service/battle_on_rosenz/tmp/pids
 bundle exec rails s -e ${ENV} -d
